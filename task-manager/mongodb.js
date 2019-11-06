3f1c3f4a1bafc7728a38d9c6a2b4336d581ddc4b
keff6 <kev.fallas@gmail.com>
2019-11-06 15:31:32 -0600

    Add mongodb driver and test

---
+++ task-manager/mongodb.js
@@ -0,0 +1,51 @@
+// CRUD
+const mongodb = require('mongodb');
+const MongoClient = mongodb.MongoClient
+
+const connectionURL = 'mongodb://127.0.0.1:27017'
+const databaseName = 'task-manager'
+
+MongoClient.connect(connectionURL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+}, (error, client) => {
+  if(error) {
+    return console.log('Unable to connect to database')
+  }
+
+  const db = client.db(databaseName);
+
+  // db.collection('users').insertOne({
+  //   name: 'Kevin',
+  //   age: 34,
+  // }, (error, result) => { 
+  //   if(error) {
+  //     return console.log('Unable to insert user')
+  //   }
+
+  //   console.log(result.ops)
+  // })
+
+  // db.collection('users').insertMany([
+  //   { name: 'Freddy', age: 58 },
+  //   { name: 'Marta', age: 57 },
+  // ], (error, result) => {
+  //   if(error) {
+  //     return console.log('Unable to insert documents')
+  //   }
+
+  //   console.log(result.ops)
+  // })
+
+  db.collection('tasks').insertMany([
+    { description: 'Kiss Princess', completed: false },
+    { description: 'Eat food', completed: true },
+    { description: 'Be successful in life', completed: false },
+  ], (error, result) => {
+    if(error) {
+      return console.log('Unable to insert documents')
+    }
+
+    console.log(result.ops)
+  })
+})
