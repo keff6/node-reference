afe50ca5623c48e31efa76b527be4a6c571f29f9
keff6 <kev.fallas@gmail.com>
2019-12-16 16:36:06 -0600

    Add tests for task suite

--- task-manager/tests/fixtures/db.js
+++ task-manager/tests/fixtures/db.js
@@ -1,6 +1,7 @@
 const jwt = require('jsonwebtoken')
 const mongoose = require('mongoose')
 const User = require('../../src/models/user')
+const Task = require('../../src/models/task')
 
 const userOneId = new mongoose.Types.ObjectId()
 const userOne = {
@@ -13,13 +14,55 @@ const userOne = {
   }]
 }
 
+const userOTwoId = new mongoose.Types.ObjectId()
+const userTwo = {
+  _id: userOTwoId,
+  name: 'Chester',
+  email: 'chester@test.com',
+  password: 'Badminton!2',
+  tokens: [{
+    token: jwt.sign({_id: userOTwoId}, process.env.JWT_SECRET)
+  }]
+}
+
+const taskOne = {
+  _id: new mongoose.Types.ObjectId(),
+  description: 'test task data',
+  completed: false,
+  owner: userOneId,
+}
+
+const taskTwo = {
+  _id: new mongoose.Types.ObjectId(),
+  description: 'test task data two',
+  completed: true,
+  owner: userOneId,
+}
+
+const taskThree = {
+  _id: new mongoose.Types.ObjectId(),
+  description: 'test task data 3',
+  completed: false,
+  owner: userTwoId,
+}
+
 const setupDatabase = async () => {
   await User.deleteMany()
+  await Task.deleteMany()
   await new User(userOne).save()
+  await new User(userTwo).save()
+  await new Task(taskOne).save()
+  await new Task(taskTwo).save()
+  await new Task(taskThree).save()
 }
 
 module.exports = {
   userOneId,
   userOne,
+  userTwoId,
+  userTwo,
+  taskOne,
+  taskTwo,
+  taskThree,
   setupDatabase,
 }
